c7c1cd3ef5e21b5189bd6048d012947e02d65296
vnshas <vnshas@gmail.com>
2024-04-01 19:15:08 -0300

    Função  playRockPaperScissor criada

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2){
+    if (player1 == 'Pedra' && player2 == 'Papel'){
+        return "Jogador 2 venceu!"
+    }
+    else if (player1 == 'Pedra' && player2 == 'Pedra'){
+        return "Empate!"
+    }
+    else if (player1 == 'Pedra' && player2 == 'Tesoura'){
+        return "Jogador 1 venceu!"
+    }
+    else if (player1 == 'Papel' && player2 == 'Pedra') {
+        return "Jogador 1 venceu!"
+    }
+    else if (player1 == 'Papel' && player2 == 'Tesoura'){
+        return "Jogador 2 venceu!"
+    }
+    else if (player1 == 'Papel' && player2 == 'Papel' ){
+        return "Empate!"
+    }
+    else if (player1 == 'Tesoura' && player2 == 'Pedra'){
+        return "Jogador 2 venceu!"
+    }
+    else if (player1 == 'Tesoura' && player2 == 'Papel' ){
+        return "Jogador 1 venceu!"
+    }
+    else if (player1 == 'Tesoura' && player2 == 'Tesoura'){
+        return "Empate!"
+    }
+    return "Dados inválidos!"
+    
+}
